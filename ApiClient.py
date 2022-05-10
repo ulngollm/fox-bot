fb8adea6b68d49a59dba591a508ec1a836b7fcb8
ulngollm <ulngollm@gmail.com>
2022-05-10 16:13:19 +0300

    add new fox src and dogs

---
+++ ApiClient.py
@@ -0,0 +1,54 @@
+import requests as req
+import random
+
+
+class ApiClient:
+    def __init__(self, url):
+        self.url = url
+
+    def random(self):
+        pass
+
+
+class RandomFoxApiClient(ApiClient):
+    def __init__(self):
+        url = 'https://randomfox.ca/floof/'
+        super().__init__(url)
+
+    def random(self):
+        result = req.get(self.url)
+        return result.json().get('image')
+
+
+class DogApiClient(ApiClient):
+    """
+    https://dog.ceo/dog-api/
+    Get concrete dog https://dog.ceo/dog-api/breeds-list
+    """
+
+    def __init__(self):
+        url = 'https://dog.ceo/api/breeds/image/random'
+        super().__init__(url)
+
+    def random(self):
+        result = req.get(self.url)
+        return result.json().get('message')
+
+
+class RandomFoxApiClient(ApiClient):
+    def __init__(self):
+        url = 'https://randomfox.ca/floof/'
+        super().__init__(url)
+
+    def random(self):
+        result = req.get(self.url)
+        return result.json().get('image')
+
+
+class FoxrudorApiClient(ApiClient):
+    def __init__(self):
+        url = 'https://foxrudor.de/'
+        super().__init__(url)
+
+    def get(self):
+        return self.url
